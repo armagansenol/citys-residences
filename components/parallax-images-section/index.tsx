76d8c2418b330867b1452df488d1317bcb957cfb
armagansenol <armagansnl@gmail.com>
2025-04-14 17:14:37 +0300

    refactor: Update parallax image section styles for improved layout
    - Removed unnecessary class from the parallax text container for cleaner markup.
    - Adjusted tablet font size in CSS for the title to enhance readability across devices.

--- components/parallax-images-section/index.tsx
+++ components/parallax-images-section/index.tsx
@@ -37,7 +37,7 @@ export function MaskedParallaxImageSection({
       <div className="flex flex-col-reverse bt:grid bt:grid-cols-24 bt:items-center gap-6 bt:gap-0">
         <div
           className={cn(
-            "gsap-parallax-text col-span-7 flex flex-col gap-12",
+            "col-span-7 flex flex-col gap-12",
             horizontalAlignment === "ltr" ? "col-start-1 order-2 bt:order-1" : "col-start-[18] order-1 bt:order-2"
           )}
         >
