75dcf488c126eebd45bdfc54c43d908ada078258
armagansenol <armagansnl@gmail.com>
2025-06-27 09:50:04 +0300

    refactor: Simplify FullScreenSlider component and enhance styles
    - Removed unused GSAP animation code and refactored the structure for clarity.
    - Updated title and description classes for improved styling and responsiveness.
    - Enhanced visual effects with new background and text-shadow properties for better aesthetics.

--- components/full-screen-slider/index.tsx
+++ components/full-screen-slider/index.tsx
@@ -3,7 +3,6 @@
 import s from "./full-screen-slider.module.css"
 
 import { cn } from "@/lib/utils"
-import { useRef } from "react"
 
 import { EmblaCarousel } from "@/components/utility/embla-carousel"
 
@@ -14,77 +13,27 @@ export interface FullScreenSliderProps {
 }
 
 export function FullScreenSlider({ title, description, items }: FullScreenSliderProps) {
-  const ref = useRef(null)
-
-  // useGSAP(
-  //   () => {
-  //     gsap.registerPlugin(ScrollTrigger)
-
-  //     // text timeline
-  //     const textTL = gsap.timeline({ paused: true })
-
-  //     textTL.from(".gsap-title", {
-  //       yPercent: -150,
-  //       ease: "expo.out",
-  //       duration: 1.5,
-  //     })
-
-  //     ScrollTrigger.create({
-  //       animation: textTL,
-  //       trigger: ".gsap-title-c",
-  //       start: "center center",
-  //       toggleActions: "play none none pause",
-  //     })
-
-  //     // card timeline
-  //     const cardTL = gsap.timeline({ paused: true })
-
-  //     cardTL.from(".gsap-description", {
-  //       yPercent: 30,
-  //       opacity: 0,
-  //       ease: "expo.out",
-  //       duration: 1.5,
-  //     })
-
-  //     ScrollTrigger.create({
-  //       animation: cardTL,
-  //       trigger: ".gsap-description-c",
-  //       toggleActions: "play none none pause",
-  //     })
-  //   },
-  //   {
-  //     scope: ref,
-  //   }
-  // )
-
   return (
-    <div className="relative flex flex-col bt:flex-row bt:overflow-hidden gap-5 w-screen z-[100]" ref={ref}>
-      <div className={cn(s["title-c"], "gsap-title-c", "z-[150]")}>
-        <h2
-          className={cn(
-            s.title,
-            "gsap-title",
-            "text-bricky-brick bt:text-white font-suisse-intl font-medium leading-tight bt:leading-none text-center"
-          )}
-        >
-          {title}
-        </h2>
-      </div>
+    <div className="relative flex flex-col bt:flex-row bt:overflow-hidden gap-6 w-screen z-[100]">
       <div className="w-screen relative">
         <EmblaCarousel autoplay={true} autoplayDelay={5000} slides={items} options={{ duration: 35, loop: true }} />
       </div>
-      <div className={cn(s.descriptionC, "gsap-description-c")}>
-        <div className={cn(s.description, "gsap-description", "w-full h-full rounded-lg overflow-hidden")}>
-          <p
-            className={cn(
-              s.infoText,
-              "font-suisse-intl text-black bt:text-white font-normal leading-relaxed text-center bd:text-left z-[150]"
-            )}
-          >
-            {description}
-          </p>
-        </div>
-      </div>
+      <h2
+        className={cn(
+          s.title,
+          "text-bricky-brick lg:text-white text-3xl lg:text-7xl font-suisse-intl font-medium leading-tight lg:leading-none text-left lg:text-center z-50 section-container"
+        )}
+      >
+        {title}
+      </h2>
+      <p
+        className={cn(
+          s.description,
+          "font-suisse-intl text-black lg:text-white text-base font-normal leading-relaxed w-full lg:w-4/12 px-6 lg:px-4 lg:py-4 rounded-xl"
+        )}
+      >
+        {description}
+      </p>
     </div>
   )
 }
