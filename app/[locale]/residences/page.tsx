9a2c040a209b26845214ca5b55049050b4e10610
armagansenol <armagansnl@gmail.com>
2025-08-01 13:54:32 +0300

    style: Update text sizes and layout in residences page for improved readability and visual consistency
    - Adjusted text sizes for headings and paragraphs to enhance readability across different screen sizes.
    - Modified layout properties to ensure better alignment and spacing of elements within the section.

--- app/[locale]/residences/page.tsx
+++ app/[locale]/residences/page.tsx
@@ -48,18 +48,18 @@ export default async function Page({ params: { locale } }: { params: { locale: s
       </section>
       <AnimatedLine direction="horizontal" />
       <section className="hidden xl:block bg-white relative z-30 section-container">
-        <div className="relative flex flex-col items-center justify-center max-w-4xl mx-auto py-32 pb-0">
-          <h2 className="font-primary font-bold text-bricky-brick text-4xl lg:text-7xl xl:text-5xl 2xl:text-5xl 3xl:text-6xl 2xl:leading-tight 3xl:leading-tight mb-5 lg:mb-10 text-center xl:text-center">
+        <div className="relative flex flex-col items-center justify-center lg:max-w-4xl 3xl:max-w-5xl mx-auto py-32 pb-0">
+          <h2 className="font-primary font-bold text-bricky-brick text-4xl lg:text-7xl xl:text-6xl 2xl:text-6xl 3xl:text-7xl xl:leading-tight 2xl:leading-snug 3xl:leading-snug mb-6 text-center xl:text-center">
             <GsapSplitText splitBy="lines" stagger={0.05} duration={1}>
               Yaşama Alan Açan Detaylar
             </GsapSplitText>
           </h2>
-          <p className="font-primary font-medium text-md lg:text-4xl xl:text-4xl 2xl:text-4xl 3xl:text-5xl xl:leading-normal 3xl:leading-normal mb-4 text-center xl:text-center">
+          <p className="font-primary font-medium text-md lg:text-4xl xl:text-4xl 2xl:text-4xl 3xl:text-5xl xl:leading-snug 2xl:leading-normal 3xl:leading-normal mb-6 text-center xl:text-center">
             <GsapSplitText splitBy="lines" stagger={0.05} duration={1}>
               Her metrekaresi ince tasarlanmış, <br /> ferah bir hayata açılan çizgiler
             </GsapSplitText>
           </p>
-          <p className="font-primary font-normal text-md lg:text-4xl xl:text-2xl 2xl:text-3xl 3xl:text-3xl xl:leading-normal 3xl:leading-normal text-center xl:text-center">
+          <p className="font-primary font-normal text-md lg:text-4xl xl:text-3xl 2xl:text-3xl 3xl:text-3xl xl:leading-normal 2xl:leading-normal 3xl:leading-normal text-center xl:text-center">
             <GsapSplitText splitBy="lines" stagger={0.05} duration={1}>
               Günlük alışkanlıklardan uzun vadeli konfora kadar her detay, yaşamın doğal akışına uyum sağlayacak şekilde
               tasarlandı.
