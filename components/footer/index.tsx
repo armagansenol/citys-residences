41d3e72ffeb1a79a06e62caa344d2bb835dac757
armagansenol <armagansnl@gmail.com>
2025-08-12 10:09:52 +0300

    refactor: Revise footer layout and typography for enhanced responsiveness and visual clarity
    - Updated footer component styles for improved text sizing and alignment across different screen sizes.
    - Adjusted padding and margins for better spacing and layout consistency.
    - Enhanced accessibility by refining class names and ensuring proper text contrast.

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,29 +1,29 @@
 "use client"
 
 import { cn } from "@/lib/utils"
+import { useLenis } from "lenis/react"
 import { useLocale, useTranslations } from "next-intl"
 import { useEffect, useRef } from "react"
 import { useIntersection } from "react-use"
 
+import { gsap } from "@/components/gsap"
 import { Logo } from "@/components/icons"
+import { ScrollToTop } from "@/components/scroll-to-top"
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
 import { Link as LocalizedLink } from "@/components/utility/link"
 import { Locale, routing } from "@/i18n/routing"
+import { getNavigationItems } from "@/lib/constants"
 import { useVisibilityStore } from "@/lib/store/visibility"
 import { colors } from "@/styles/config.mjs"
-import { ScrollToTop } from "../scroll-to-top"
-import { getNavigationItems } from "@/lib/constants"
-import { useLenis } from "lenis/react"
-import { gsap } from "@/components/gsap"
 
 const styles = {
   // Text size patterns
   textSizes: {
     linkTextSm: "text-base lg:text-xs xl:text-sm",
-    linkText: "text-base lg:text-xs xl:text-lg 2xl:text-xl",
-    headingText: "text-lg lg:text-sm xl:text-2xl 2xl:text-3xl text-white",
-    contactText: "text-base lg:text-sm xl:text-lg",
-    copyrightCredit: "text-[0.8rem] lg:text-xs xl:text-base text-white",
+    linkText: "text-lg lg:text-xs xl:text-lg 2xl:text-xl",
+    headingText: "text-3xl lg:text-sm xl:text-2xl 2xl:text-3xl",
+    contactText: "text-lg lg:text-sm xl:text-lg",
+    copyrightCredit: "text-[0.8rem] lg:text-xs xl:text-base",
     mobileAccordionLink: "text-base lg:text-xs xl:text-sm",
     mobileAccordionHeading: "text-lg lg:text-sm xl:text-base",
   },
@@ -105,24 +105,27 @@ export function Footer() {
   }
 
   return (
-    <footer className="relative bg-bricky-brick py-12 xl:py-10 xl:pt-2" ref={footerRef}>
-      <div className="section-container flex flex-col gap-12 lg:gap-0 pt-12">
-        <div className="flex flex-col-reverse items-stretch lg:flex-row lg:items-center gap-12 lg:gap-0">
+    <footer className="relative bg-bricky-brick py-12 xl:py-10 xl:pt-2 text-white" ref={footerRef}>
+      <div className="section-container flex flex-col gap-12 lg:gap-0 pt-0 lg:pt-12">
+        <div className="flex flex-col-reverse items-stretch lg:flex-row lg:items-center gap-12 lg:gap-0 px-5 lg:px-0">
           {/* Logo Section */}
-          <div className="w-full lg:w-3/12 flex items-center justify-center mr-auto">
-            <LocalizedLink href="/" className="w-[200px] lg:w-[200px] xl:w-[260px] 2xl:w-[300px]">
+          <div className="order-1 lg:order-none w-full lg:w-3/12 flex items-center justify-center mr-auto">
+            <LocalizedLink
+              href="/"
+              className="w-[240px] sm:w-[200px] lg:w-[200px] xl:w-[260px] 2xl:w-[300px] max-w-full"
+            >
               <Logo fill={colors["white"]} />
             </LocalizedLink>
           </div>
-          <div className="w-full lg:w-8/12 flex flex-col items-stretch pb-7 lg:pb-0 relative">
-            <ScrollToTop className={cn("text-3xl 3xl:text-4xl ml-auto lg:w-3/12")} />
+          <div className="w-full lg:w-8/12 flex flex-col-reverse lg:flex-col items-stretch pb-7 lg:pb-0 relative">
+            <ScrollToTop className={cn("text-3xl 3xl:text-4xl ml-auto lg:w-3/12 mx-auto lg:mx-0 mt-8 lg:mt-0")} />
             <div className="flex flex-col lg:flex-row">
               {/* Contact Section */}
               <div className="w-full lg:w-4/12 py-10 lg:py-0">
                 <h5 className={cn(styles.textSizes.headingText, styles.layout.sectionHeader)}>{t("contact")}</h5>
-                <div className="flex flex-col items-stretch gap-6 mr-0 xl:mr-10 pr-10">
+                <div className="flex flex-col items-stretch gap-6 mr-0 xl:mr-10 pr-0 lg:pr-10">
                   <div className="space-y-4">
-                    <span className={cn("block", styles.textSizes.contactText, "text-white whitespace-pre-line")}>
+                    <span className={cn("block", styles.textSizes.contactText, "whitespace-pre-line")}>
                       {t.rich("contactInfo", {
                         br: () => <br />,
                       })}
@@ -131,7 +134,7 @@ export function Footer() {
                       href="https://maps.app.goo.gl/2hSJUsgo2U198Kqq9"
                       className={cn(
                         "block",
-                        "text-white whitespace-pre-line",
+                        "whitespace-pre-line",
                         styles.textSizes.linkText,
                         styles.interactions.opacityHover,
                         "xl:leading-tight"
@@ -139,16 +142,16 @@ export function Footer() {
                       target="_blank"
                       rel="noopener noreferrer"
                     >
-                      <span className="block font-bold whitespace-nowrap">City’s İstanbul AVM</span>
-                      <span className="block whitespace-nowrap">İçerenköy, Çayır Cd No: 1,</span>
-                      <span className="block whitespace-nowrap">34752 Ataşehir/Istanbul</span>
+                      <span className="block font-bold sm:whitespace-nowrap">City’s İstanbul AVM</span>
+                      <span className="block sm:whitespace-nowrap">İçerenköy, Çayır Cd No: 1,</span>
+                      <span className="block sm:whitespace-nowrap">34752 Ataşehir/Istanbul</span>
                     </a>
                     <a
                       href="mailto:info@citysresidences.com"
                       className={cn(
                         "block",
                         styles.textSizes.linkText,
-                        "text-white whitespace-pre-line",
+                        "whitespace-pre-line",
                         styles.interactions.opacityHover
                       )}
                     >
@@ -158,7 +161,7 @@ export function Footer() {
                       href="tel:+902162666600"
                       className={cn(
                         "block",
-                        "text-white font-bold whitespace-pre-line",
+                        "font-bold whitespace-pre-line",
                         styles.textSizes.linkText,
                         styles.interactions.opacityHover
                       )}
@@ -212,16 +215,14 @@ export function Footer() {
                 </Accordion>
               </div>
               {/* Legal Section */}
-              <div className="lg:w-3/12 ml-auto opacity-60">
+              <div className="w-full lg:w-3/12 ml-auto opacity-60">
                 {/* desktop */}
                 <div className={styles.layout.desktopOnly}>
                   <h5 className={cn(styles.textSizes.headingText, styles.layout.sectionHeader, "opacity-0")}>
                     {t("legal")}
                   </h5>
                   <div className="space-y-2">
-                    <h5
-                      className={cn("block", styles.textSizes.linkTextSm, styles.interactions.linkHover, "text-white")}
-                    >
+                    <h5 className={cn("block", styles.textSizes.linkTextSm, styles.interactions.linkHover)}>
                       {t("legal")}
                     </h5>
                     {footerItems.legal.map((item, i) => (
@@ -265,63 +266,18 @@ export function Footer() {
           </div>
         </div>
         {/* Copyright Section */}
-        <div className="flex flex-col lg:flex-row justify-between items-end gap-5 relative mt-14">
+        <div className="flex flex-col lg:flex-row justify-between items-center lg:items-end gap-5 relative lg:mt-14">
           <span
             className={cn(
               styles.textSizes.copyrightCredit,
-              "block absolute left-1/2 -translate-x-1/2 bottom-0 text-center"
+              "block text-center lg:absolute lg:left-1/2 lg:-translate-x-1/2 lg:bottom-0"
             )}
           >
             {t("copyright")}
           </span>
-          {/* <div className="w-full lg:w-3/12 flex flex-col items-center justify-center">
-            <div className="flex flex-col items-start justify-center">
-              <span className={cn(styles.textSizes.copyrightCredit, "mr-auto")}>Bizi Takip Edin</span>
-              <div className="flex items-center justify-center gap-6 lg:gap-6 pt-1 mt-1.5 border-t-[3px] border-white/40">
-                <div
-                  className={cn(
-                    styles.iconSizes.social,
-                    styles.interactions.opacityHover,
-                    styles.interactions.socialIcon
-                  )}
-                >
-                  {socialIcons.instagram}
-                </div>
-                <div
-                  className={cn(
-                    styles.iconSizes.social,
-                    styles.interactions.opacityHover,
-                    styles.interactions.socialIcon
-                  )}
-                >
-                  {socialIcons.facebook}
-                </div>
-                <div
-                  className={cn(
-                    styles.iconSizes.social,
-                    styles.interactions.opacityHover,
-                    styles.interactions.socialIcon
-                  )}
-                >
-                  {socialIcons.tiktok}
-                </div>
-                <div
-                  className={cn(
-                    styles.iconSizes.social,
-                    styles.interactions.opacityHover,
-                    styles.interactions.socialIcon,
-                    "xl:h-9 xl:w-9"
-                  )}
-                >
-                  {socialIcons.youtube}
-                </div>
-              </div>
-            </div>
-          </div> */}
           <div className="w-full">
             <div className="flex flex-col lg:flex-row items-center lg:items-start justify-between gap-5">
-              {/* <ScrollToTop className={cn(styles.textSizes.copyrightCredit, "ml-auto")} /> */}
-              <span className={cn(styles.textSizes.copyrightCredit, "ml-auto")}>
+              <span className={cn(styles.textSizes.copyrightCredit, "lg:ml-auto")}>
                 Made by{" "}
                 <LocalizedLink
                   href="https://justdesignfx.com"
