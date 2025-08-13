741fdc9d562ef67970ffd998d2159e191d70d030
armagansenol <armagansnl@gmail.com>
2025-08-13 15:16:01 +0300

    refactor: Update PageTitle component for improved typography and alignment
    - Adjusted text alignment to center for better visual presentation.
    - Modified font size for improved readability across different screen sizes.
    - Enhanced overall styling consistency within the PageTitle component.

--- components/page-title/index.tsx
+++ components/page-title/index.tsx
@@ -13,8 +13,8 @@ export function PageTitle(props: PageTitleProps) {
       <FadeInOnScroll>
         <h2
           className={cn(
-            "font-montserrat font-semibold text-bricky-brick",
-            "text-5xl lg:text-5xl xl:text-7xl 2xl:text-7xl",
+            "font-montserrat font-semibold text-bricky-brick text-center",
+            "text-4xl lg:text-5xl xl:text-7xl 2xl:text-7xl",
             "leading-tight lg:leading-tight xl:leading-tight 2xl:leading-tight",
             props.textClassName
           )}
