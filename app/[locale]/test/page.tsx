f4d8e7c4b6aa897412b24e32976423f28c86be4d
armagansenol <armagansnl@gmail.com>
2025-10-19 21:24:13 +0300

    refactor(test-page): standardize color property usage and remove unused components
    - Update color properties to use single quotes for consistency
    - Remove unused VideoWithText components to streamline the TestPage layout
    - Enhance overall readability and maintainability of the TestPage component

--- app/[locale]/test/page.tsx
+++ app/[locale]/test/page.tsx
@@ -1,10 +1,8 @@
-import { Footer } from "@/components/footer"
-import { Header } from "@/components/header"
-import { HeroSection } from "@/components/hero-section"
-import { PageTitle } from "@/components/page-title"
-import { VideoWithText } from "@/components/video-with-text"
-import { citysParkVideo } from "@/lib/constants"
-import { colors } from "@/styles/config.mjs"
+import { Footer } from '@/components/footer'
+import { Header } from '@/components/header'
+import { HeroSection } from '@/components/hero-section'
+import { PageTitle } from '@/components/page-title'
+import { colors } from '@/styles/config.mjs'
 
 export default function TestPage() {
   return (
@@ -12,33 +10,20 @@ export default function TestPage() {
       <Header />
       <PageTitle
         className='bg-army-canvas'
-        bgColor={colors["army-canvas"]}
+        bgColor={colors['army-canvas']}
         itemColor={colors.white}
         title="CITY'S PARK"
         description='Şehrin kalbinde, sizi yavaşlatan, yeşil bir vaha...'
         id='test'
       />
       <PageTitle
-        bgColor={colors["bricky-brick"]}
+        bgColor={colors['bricky-brick']}
         title='DAiRELER'
         description='Günlük yaşamın alışkanlıklarından, yıllara yayılan huzurlu anılara kadar her detay; evinizin size ait bir dünyaya dönüşmesi için tasarlandı.'
         id='test'
       />
-      <VideoWithText
-        primaryVideoUrl={citysParkVideo}
-        title={
-          <span>
-            Daha<strong> huzurlu </strong>yaşa.
-          </span>
-        }
-        description={
-          <>
-            Farklı ve zamansız mimarinin, doğanın cömertliği ile buluştuğu mekanlarda güven içinde, daha huzurlu yaşa.
-          </>
-        }
-      />
       <PageTitle
-        bgColor={colors["blue-shimmer"]}
+        bgColor={colors['blue-shimmer']}
         className='bg-blue-shimmer'
         title={
           <>
@@ -50,22 +35,8 @@ export default function TestPage() {
         itemColor={colors.black}
         id='test'
       />
-      <VideoWithText
-        primaryVideoUrl={citysParkVideo}
-        title={
-          <span>
-            Daha<strong> dolu </strong>yaşa.
-          </span>
-        }
-        description={
-          <>
-            Spor, sanat, kültür, alışveriş, eğlence ve çok daha fazlasını bir araya getiren yeni bir yaşam dizaynı ile
-            daha dolu yaşa.
-          </>
-        }
-      />
       <PageTitle
-        bgColor={colors["verve-violet"]}
+        bgColor={colors['verve-violet']}
         className='bg-verve-violet'
         title={
           <>
@@ -82,7 +53,7 @@ export default function TestPage() {
         videoThumbnail='/img/thumbnail-kolaj-video.jpg'
       />
       <PageTitle
-        bgColor={colors["aqua-belt"]}
+        bgColor={colors['aqua-belt']}
         className='bg-aqua-belt'
         title={
           <>
@@ -95,14 +66,13 @@ export default function TestPage() {
         id='test'
       />
       <PageTitle
-        bgColor={colors["trapped-darkness"]}
+        bgColor={colors['trapped-darkness']}
         className='bg-trapped-darkness'
         title="CITY'S TIMES"
         description='Bizi takip edin.'
         itemColor={colors.white}
         id='test'
       />
-
       <Footer />
     </div>
   )
