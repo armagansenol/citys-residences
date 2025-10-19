f573003fe6820ed92e479df881af6ed880f151b9
armagansenol <armagansnl@gmail.com>
2025-10-19 22:01:03 +0300

    refactor(menu): standardize quotes and improve code readability
    - Update import statements and string literals to use single quotes for consistency
    - Refactor layout structure for enhanced readability and maintainability
    - Adjust spacing and formatting for better visual clarity

--- components/menu/index.tsx
+++ components/menu/index.tsx
@@ -1,19 +1,19 @@
-"use client"
+'use client'
 
-import { gsap, useGSAP } from "@/components/gsap"
-import { cn } from "@/lib/utils"
-import { useLenis } from "lenis/react"
-import { ChevronRight } from "lucide-react"
-import { useRef } from "react"
-import { useClickAway } from "react-use"
+import { gsap, useGSAP } from '@/components/gsap'
+import { cn } from '@/lib/utils'
+import { useLenis } from 'lenis/react'
+import { ChevronRight } from 'lucide-react'
+import { useRef } from 'react'
+import { useClickAway } from 'react-use'
 
-import { IconWrapper } from "@/components/icon-wrapper"
-import { socialIcons } from "@/components/icons"
-import { Img } from "@/components/utility/img"
-import { ScrollableBox } from "@/components/utility/scrollable-box"
-import { citysIstanbulAvmGoogleMaps, NavigationMetadata } from "@/lib/constants"
-import { useScrollStore } from "@/lib/store/scroll"
-import { colors } from "@/styles/config.mjs"
+import { IconWrapper } from '@/components/icon-wrapper'
+import { socialIcons } from '@/components/icons'
+import { Img } from '@/components/utility/img'
+import { ScrollableBox } from '@/components/utility/scrollable-box'
+import { citysIstanbulAvmGoogleMaps, NavigationMetadata } from '@/lib/constants'
+import { useScrollStore } from '@/lib/store/scroll'
+import { colors } from '@/styles/config.mjs'
 
 interface MenuProps {
   items: NavigationMetadata[]
@@ -32,8 +32,8 @@ export function Menu({ items }: MenuProps) {
     smoothScrollWithWrapper,
   } = useScrollStore()
 
-  useClickAway(menuRef, (e) => {
-    if ((e.target as HTMLElement).closest("[data-ignore-click-away]")) {
+  useClickAway(menuRef, e => {
+    if ((e.target as HTMLElement).closest('[data-ignore-click-away]')) {
       return
     }
     setOpen(false)
@@ -47,11 +47,11 @@ export function Menu({ items }: MenuProps) {
 
       menuTL.current.fromTo(
         menuRef.current,
-        { translateX: "100%" },
+        { translateX: '100%' },
         {
-          translateX: "0%",
+          translateX: '0%',
           duration: 0.8,
-          ease: "expo.inOut",
+          ease: 'expo.inOut',
         }
       )
     },
@@ -77,11 +77,13 @@ export function Menu({ items }: MenuProps) {
   )
 
   const handleScroll = (id: string) => {
-    if (id.includes("stacking-cards")) {
-      const cardIndex = parseInt(id.split("-")[2])
+    if (id.includes('stacking-cards')) {
+      const cardIndex = parseInt(id.split('-')[2])
       smoothScrollWithWrapper(() => scrollToCard(cardIndex, true))
     } else {
-      smoothScrollWithWrapper(() => lenis?.scrollTo(`#${id}`, { immediate: true }))
+      smoothScrollWithWrapper(() =>
+        lenis?.scrollTo(`#${id}`, { immediate: true })
+      )
     }
   }
 
@@ -93,42 +95,47 @@ export function Menu({ items }: MenuProps) {
     <>
       <div
         className={cn(
-          "fixed top-0 right-0 bottom-0 z-[var(--z-menu)] translate-x-full",
-          "w-screen lg:w-[60vw]",
-          "blur-bg-white-2",
-          "pt-20 pl-24 pr-16 pb-10"
+          'fixed bottom-0 right-0 top-0 z-[var(--z-menu)] translate-x-full',
+          'w-screen lg:w-[60vw]',
+          'blur-bg-white-2',
+          'pb-10 pl-24 pr-16 pt-20'
         )}
         ref={menuRef}
         data-ignore-click-away
       >
         <button
           className={cn(
-            "absolute top-2 lg:top-20 left-0 w-16 h-16 z-16 -translate-x-full bg-white p-2 text-bricky-brick",
-            "opacity-0 transition-opacity duration-700 ease-in-out",
-            "flex items-center justify-center",
+            'z-16 absolute left-0 top-2 h-16 w-16 -translate-x-full bg-white p-2 text-bricky-brick lg:top-20',
+            'opacity-0 transition-opacity duration-700 ease-in-out',
+            'flex items-center justify-center',
             {
-              "opacity-100": open,
+              'opacity-100': open,
             }
           )}
           onClick={() => setOpen(false)}
           type='button'
         >
-          <ChevronRight className='w-8 h-8' />
+          <ChevronRight className='h-8 w-8' />
           <span className='sr-only'>Close</span>
         </button>
         <div className='flex justify-between gap-24'>
           <div className='flex lg:items-end' data-lenis-prevent>
             <ScrollableBox>
-              <nav className='w-full h-full flex items-end justify-center lg:justify-start'>
-                <ul className={cn("flex flex-col items-stretch gap-3 lg:gap-3 xl:gap-3 2xl:gap-4 3xl:gap-4 w-full")}>
-                  {items.map((item) => (
+              <nav className='flex h-full w-full items-end justify-center lg:justify-start'>
+                <ul
+                  className={cn(
+                    'flex w-full flex-col items-stretch gap-3 lg:gap-3 xl:gap-3 2xl:gap-4 3xl:gap-4'
+                  )}
+                >
+                  {items.map(item => (
                     <li
                       className={cn(
-                        "flex items-center justify-between gap-2",
-                        "text-xl lg:text-xl xl:text-xl 2xl:text-2xl 3xl:text-2xl",
-                        "font-primary font-[300] text-white text-center lg:text-left",
+                        'flex items-center justify-between gap-2',
+                        'text-xl lg:text-xl xl:text-xl 2xl:text-2xl 3xl:text-2xl',
+                        'text-center font-primary font-[300] text-white lg:text-left',
                         {
-                          "text-6xl lg:text-6xl xl:text-6xl 2xl:text-6xl 3xl:text-6xl": item.mainRoute,
+                          'text-6xl lg:text-6xl xl:text-6xl 2xl:text-6xl 3xl:text-6xl':
+                            item.mainRoute,
                         }
                       )}
                       key={item.title}
@@ -145,67 +152,83 @@ export function Menu({ items }: MenuProps) {
               </nav>
             </ScrollableBox>
           </div>
-          <div className='flex flex-col flex-1 gap-8'>
-            <div className='h-72 w-full relative overflow-hidden'>
+          <div className='flex flex-1 flex-col gap-8'>
+            <div className='relative h-72 w-full overflow-hidden'>
               <Img
                 src='/img/citys-istanbul-avm-hero.jpg'
                 alt="City's Istanbul AVM"
                 fill
-                sizes='100vw'
+                sizes='40vw'
                 loading='lazy'
                 className='object-cover'
               />
             </div>
             <p
               className={cn(
-                "flex items-center justify-between gap-2",
-                "text-xl lg:text-xl xl:text-xl 2xl:text-2xl 3xl:text-2xl",
-                "font-primary font-[300] text-white text-center lg:text-left"
+                'flex items-center justify-between gap-2',
+                'text-xl lg:text-xl xl:text-xl 2xl:text-2xl 3xl:text-2xl',
+                'text-center font-primary font-[300] text-white lg:text-left'
               )}
             >
-              Hayatın tam merkezinde, zamanı kendinize ve sevdiklerinize ayırarak keyifle, yaşamı sanata dönüştürerek
-              daha çok yaşa.
+              Hayatın tam merkezinde, zamanı kendinize ve sevdiklerinize
+              ayırarak keyifle, yaşamı sanata dönüştürerek daha çok yaşa.
             </p>
           </div>
         </div>
         <div className='flex items-end justify-between'>
           <div className='flex flex-col items-start'>
-            <div className='grid grid-cols-4 gap-2 lg:gap-2 xl:gap-2 w-[300px]'>
+            <div className='grid w-[300px] grid-cols-4 gap-2 lg:gap-2 xl:gap-2'>
               <IconWrapper
-                className={cn("w-full aspect-square opacity-70 transition-opacity cursor-pointer", "hover:opacity-100")}
+                className={cn(
+                  'aspect-square w-full cursor-pointer opacity-70 transition-opacity',
+                  'hover:opacity-100'
+                )}
               >
                 {socialIcons(colors.white).instagram}
               </IconWrapper>
               <IconWrapper
-                className={cn("w-full aspect-square opacity-70 transition-opacity cursor-pointer", "hover:opacity-100")}
+                className={cn(
+                  'aspect-square w-full cursor-pointer opacity-70 transition-opacity',
+                  'hover:opacity-100'
+                )}
               >
                 {socialIcons(colors.white).facebook}
               </IconWrapper>
               <IconWrapper
-                className={cn("w-full aspect-square opacity-70 transition-opacity cursor-pointer", "hover:opacity-100")}
+                className={cn(
+                  'aspect-square w-full cursor-pointer opacity-70 transition-opacity',
+                  'hover:opacity-100'
+                )}
               >
                 {socialIcons(colors.white).tiktok}
               </IconWrapper>
               <IconWrapper
-                className={cn("w-full aspect-square opacity-70 transition-opacity cursor-pointer", "hover:opacity-100")}
+                className={cn(
+                  'aspect-square w-full cursor-pointer opacity-70 transition-opacity',
+                  'hover:opacity-100'
+                )}
               >
                 {socialIcons(colors.white).youtube}
               </IconWrapper>
             </div>
           </div>
           <div className='flex items-center justify-between gap-2'>
-            <div className='w-40 h-40 border-gradient-white rounded-sm'>Randevu Oluştur</div>
-            <div className='w-40 h-40 border-gradient-white rounded-sm'>Temsilciyle Görüş</div>
+            <div className='border-gradient-white h-40 w-40 rounded-sm'>
+              Randevu Oluştur
+            </div>
+            <div className='border-gradient-white h-40 w-40 rounded-sm'>
+              Temsilciyle Görüş
+            </div>
             <a
               href={citysIstanbulAvmGoogleMaps}
               target='_blank'
               rel='noopener noreferrer'
               className={cn(
-                "block",
-                "transition-opacity duration-300 ease-in-out",
-                "opacity-100 hover:opacity-70",
-                "flex items-start gap-1",
-                "w-40 h-40 border-gradient-white rounded-sm"
+                'block',
+                'transition-opacity duration-300 ease-in-out',
+                'opacity-100 hover:opacity-70',
+                'flex items-start gap-1',
+                'border-gradient-white h-40 w-40 rounded-sm'
               )}
             >
               Yol Tarifi Al
