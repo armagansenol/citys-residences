038732f379e31d9e64d8225859f1a90e9c2c2b14
armagansenol <armagansnl@gmail.com>
2025-10-27 17:02:14 +0300

    refactor: enhance QuoteWithVideo component layout and styling
    - Update grid layout and spacing for improved responsiveness
    - Refactor class names for consistency and clarity
    - Adjust video and portrait image positioning
    - Improve accessibility and visual hierarchy of text elements

--- components/quote-with-video/index.tsx
+++ components/quote-with-video/index.tsx
@@ -37,111 +37,119 @@ export function QuoteWithVideo({
   hasBg = false,
 }: QuoteWithVideoProps) {
   return (
-    <section className={cn('relative min-h-lvh', className)}>
-      <div
-        className={cn(
-          'relative z-10 grid min-h-lvh grid-cols-24',
-          hasBg &&
-            'bg-[url(/svg/murat-kader-bg.svg)] bg-contain bg-right-bottom bg-no-repeat bg-blend-soft-light'
-        )}
-        style={{ backgroundColor: primaryColor }}
-      >
-        <div className='col-span-24 flex items-center justify-between px-8 pt-48 lg:col-span-15 lg:col-start-5 lg:px-0 lg:pt-48'>
-          <div className='max-w-[80%] text-left lg:max-w-[50%]'>
-            <h2
-              className='relative font-primary text-2xl font-[400] lg:text-4xl lg:leading-snug'
+    <section
+      className={cn(
+        'relative z-10 grid min-h-screen grid-cols-24 overflow-hidden',
+        hasBg &&
+          'bg-[url(/svg/murat-kader-bg.svg)] bg-contain bg-right-bottom bg-no-repeat bg-blend-soft-light',
+        className
+      )}
+      style={{ backgroundColor: primaryColor }}
+    >
+      {/* quote and person name and title */}
+      <div className='col-span-24 flex items-center justify-between px-8 pt-28 lg:col-span-16 lg:col-start-6 lg:px-0 lg:pt-48'>
+        <h2
+          className={cn(
+            'relative',
+            'text-left font-primary font-[400]',
+            'w-[80%] lg:w-[50vw]',
+            'text-2xl/snug lg:text-4xl/snug'
+          )}
+          style={{ color: secondaryColor }}
+        >
+          <span className='absolute left-0 top-0 z-20 block -translate-y-[140%]'>
+            <QuotesIcon
+              size={64}
+              weight='thin'
+              style={{ color: secondaryColor }}
+            />
+          </span>
+          <GsapSplitText type='lines' stagger={0.1} duration={1.5}>
+            {quote}
+          </GsapSplitText>
+        </h2>
+        {/* person name and title desktop */}
+        <div className='mt-auto hidden text-right lg:block'>
+          {personName && (
+            <h3
+              className='font-primary text-xl font-[400] lg:text-2xl'
               style={{ color: secondaryColor }}
             >
-              <span className='absolute left-0 top-0 z-20 block -translate-y-[140%]'>
-                <QuotesIcon
-                  size={64}
-                  weight='thin'
-                  style={{ color: secondaryColor }}
-                />
-              </span>
-              <GsapSplitText type='lines' stagger={0.1} duration={1.5}>
-                {quote}
-              </GsapSplitText>
-            </h2>
-          </div>
-          <div className='mt-auto hidden text-right lg:block'>
-            {personName && (
-              <h3
-                className='font-primary text-xl font-[400] lg:text-2xl'
-                style={{ color: secondaryColor }}
-              >
-                {personName}
-              </h3>
-            )}
-            {personTitle && (
-              <p
-                className='mt-1 font-primary text-sm font-[300] lg:text-2xl'
-                style={{ color: secondaryColor }}
-              >
-                {personTitle}
-              </p>
-            )}
-          </div>
+              {personName}
+            </h3>
+          )}
+          {personTitle && (
+            <p
+              className='mt-1 font-primary text-sm font-[300] lg:text-2xl'
+              style={{ color: secondaryColor }}
+            >
+              {personTitle}
+            </p>
+          )}
         </div>
-        <div className='col-span-24 px-8 pb-32 pt-16 lg:col-span-15 lg:col-start-5 lg:px-0 lg:pb-32 lg:pt-16'>
-          <div className='group relative aspect-[16/9] w-full cursor-pointer overflow-hidden rounded-md text-red-400'>
-            <FullScreenVideoDialog
-              dialogTrigger={
-                <Image
-                  src={thumbnail || ''}
-                  alt='Thumbnail'
-                  fill
-                  className='object-cover object-center'
-                />
-              }
-              mediaId={mediaId}
-            />
-            <span className='pointer-events-none absolute left-1/2 top-1/2 z-50 -translate-x-1/2 -translate-y-1/2 text-white transition-transform duration-300 ease-in-out group-hover:scale-125'>
-              <PlayCircleIcon size={120} weight='fill' />
-            </span>
-          </div>
-          <div className='mr-auto inline-flex text-right lg:hidden'>
-            {personName && (
-              <h3
-                className='font-primary text-xl font-[400] lg:text-2xl'
-                style={{ color: secondaryColor }}
-              >
-                {personName}
-              </h3>
-            )}
-            {personTitle && (
-              <p
-                className='mt-1 font-primary text-sm font-[300] lg:text-2xl'
-                style={{ color: secondaryColor }}
-              >
-                {personTitle}
-              </p>
-            )}
-          </div>
-        </div>
-        <div className='aspect-square pointer-events-none absolute bottom-0 right-0 z-50 h-[30%] w-full lg:h-2/4'>
-          <Image
-            src={portraitImage || ''}
-            alt={personName || 'Portrait'}
-            fill
-            className='object-contain object-right-bottom'
+      </div>
+      {/* video */}
+      <div className='col-span-24 border border-red-400 px-8 pb-0 pt-8 lg:col-span-16 lg:col-start-6 lg:px-0 lg:pb-16 lg:pt-16 xl:col-start-5'>
+        <div className='group relative aspect-[16/9] cursor-pointer overflow-hidden rounded-sm border border-blue-400 text-red-400'>
+          <FullScreenVideoDialog
+            dialogTrigger={
+              <Image
+                src={thumbnail ?? ''}
+                alt='Thumbnail'
+                fill
+                className='object-cover object-center'
+              />
+            }
+            mediaId={mediaId}
           />
+          <span className='pointer-events-none absolute left-1/2 top-1/2 z-50 -translate-x-1/2 -translate-y-1/2 text-white transition-transform duration-300 ease-in-out group-hover:scale-125'>
+            <PlayCircleIcon size={120} weight='fill' />
+          </span>
         </div>
-        <div className='absolute right-8 top-8 flex flex-col items-center justify-end gap-4 lg:right-24 lg:top-24'>
-          <BlueprintIcon
-            size={48}
-            weight='thin'
-            className='rotate-90'
-            style={{ color: secondaryColor }}
-          />
-          <div
-            className='writing-mode-vertical font-primary text-xl font-[400] lg:text-4xl'
-            style={{ color: secondaryColor }}
-          >
-            <span className='rotate-90 transform whitespace-nowrap'>
-              {sidebarText}
-            </span>
-          </div>
+        {/* person name and title mobile */}
+        <div className='mr-auto mt-12 inline-flex flex-col lg:mt-24 lg:hidden'>
+          {personName && (
+            <h3
+              className='max-w-[35vw] font-primary text-base/tight font-[500]'
+              style={{ color: secondaryColor }}
+            >
+              {personName}
+            </h3>
+          )}
+          {personTitle && (
+            <p
+              className='font-primary text-sm font-[300]'
+              style={{ color: secondaryColor }}
+            >
+              {personTitle}
+            </p>
+          )}
+        </div>
+      </div>
+      {/* portrait image */}
+      <div className='aspect-square pointer-events-none absolute -right-4 bottom-0 z-50 h-[35%] w-full lg:right-0 lg:h-[35%] xl:h-2/4'>
+        <Image
+          src={portraitImage || ''}
+          alt={personName || 'Portrait'}
+          fill
+          className='object-contain object-right-bottom'
+        />
+      </div>
+      {/* blueprint icon and sidebar text */}
+      <div className='absolute right-6 top-8 flex flex-col items-center justify-end gap-2 lg:right-24 lg:top-24'>
+        <BlueprintIcon
+          size={28}
+          weight='thin'
+          className='rotate-90'
+          style={{ color: secondaryColor }}
+        />
+        <div
+          className='writing-mode-vertical font-primary text-base font-[400] lg:text-4xl'
+          style={{ color: secondaryColor }}
+        >
+          <span className='rotate-90 transform whitespace-nowrap'>
+            {sidebarText}
+          </span>
         </div>
       </div>
     </section>
