c8ae05e59a653d17a4830c95fae8577b549b376c
armagansenol <armagansnl@gmail.com>
2025-10-28 16:10:58 +0300

    refactor: simplify TestPage component and enhance Footer styling
    - Remove unnecessary components and simplify TestPage to only render the Footer
    - Refactor Footer component for improved layout and responsiveness
    - Update navigation links and social icons for better accessibility and visual consistency
    - Introduce new border-radius and gradient styles in global CSS for enhanced aesthetics

--- app/[locale]/test/page.tsx
+++ app/[locale]/test/page.tsx
@@ -1,50 +1,5 @@
 import { Footer } from '@/components/footer'
-import { Header } from '@/components/header'
-import { PageTitle } from '@/components/page-title'
-import {
-  ComponentType,
-  RepetitiveSectionsWrapper,
-} from '@/components/repetitive-sections/repetitive-sections-wrapper'
-import { colors } from '@/styles/config.mjs'
 
 export default function TestPage() {
-  return (
-    <div>
-      <Header />
-      <PageTitle
-        primaryColor={colors['trapped-darkness']}
-        secondaryColor={colors.white}
-        title="CITY'S TIMES"
-        description='Bizi takip edin.'
-        id='citys-times'
-      />
-      <RepetitiveSectionsWrapper
-        componentType={ComponentType.FullWidthSingleVideo}
-        mediaId='p4l0a63nut'
-        thumbnail='/img/thumbnail-kolaj-video.jpg'
-      />
-      <RepetitiveSectionsWrapper
-        componentType={ComponentType.FullWidthVideoText}
-        title='Dynamic Zone'
-        subtitle='Hareket hiç bu kadar eğlenceli olmamıştı.'
-        description='Dilediğiniz oyunu seçin, enerjinizi ortaya koyun! Padel, basketbol, masa tenisi ve golf simülatörüyle her gününüzü farklı bir heyecana dönüştürün. Arkadaşlarınızla kıyasıya rekabet, bol kahkahalı anlar ve sınırsız enerji… Dynamic Zone’da spor sadece bir aktivite değil, unutulmaz bir deneyim. Hazır mısınız? Çünkü burada her an bir oyuna dönüşüyor.'
-        mediaId='p4l0a63nut'
-      />
-      <RepetitiveSectionsWrapper
-        componentType={ComponentType.CenterVideoText}
-        title='Dynamic Zone'
-        subtitle='Hareket hiç bu kadar eğlenceli olmamıştı.'
-        description='Dilediğiniz oyunu seçin, enerjinizi ortaya koyun! Padel, basketbol, masa tenisi ve golf simülatörüyle her gününüzü farklı bir heyecana dönüştürün. Arkadaşlarınızla kıyasıya rekabet, bol kahkahalı anlar ve sınırsız enerji… Dynamic Zone’da spor sadece bir aktivite değil, unutulmaz bir deneyim. Hazır mısınız? Çünkü burada her an bir oyuna dönüşüyor.'
-        mediaId='p4l0a63nut'
-      />
-      <RepetitiveSectionsWrapper
-        componentType={ComponentType.BackgroundVideoText}
-        title='Dynamic Zone'
-        subtitle='Hareket hiç bu kadar eğlenceli olmamıştı.'
-        description='Dilediğiniz oyunu seçin, enerjinizi ortaya koyun! Padel, basketbol, masa tenisi ve golf simülatörüyle her gününüzü farklı bir heyecana dönüştürün. Arkadaşlarınızla kıyasıya rekabet, bol kahkahalı anlar ve sınırsız enerji… Dynamic Zone’da spor sadece bir aktivite değil, unutulmaz bir deneyim. Hazır mısınız? Çünkü burada her an bir oyuna dönüşüyor.'
-        mediaId='p4l0a63nut'
-      />
-      <Footer />
-    </div>
-  )
+  return <Footer />
 }
--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,17 +1,10 @@
 'use client'
 
 import { cn } from '@/lib/utils'
-import { useLocale, useTranslations } from 'next-intl'
-import { useRef } from 'react'
-
-import { Logo } from '@/components/icons'
-import { Link as LocalizedLink } from '@/components/utility/link'
-import { Locale, routing } from '@/i18n/routing'
-import { getNavigationItems, navigationConfig } from '@/lib/constants'
-import { colors } from '@/styles/config.mjs'
 import {
   CalendarPlusIcon,
   FacebookLogoIcon,
+  HeadsetIcon,
   InstagramLogoIcon,
   MapPinAreaIcon,
   MapPinPlusIcon,
@@ -19,7 +12,15 @@ import {
   XLogoIcon,
   YoutubeLogoIcon,
 } from '@phosphor-icons/react'
+import { useLocale, useTranslations } from 'next-intl'
+import { Fragment, useRef } from 'react'
+
+import { Logo } from '@/components/icons'
+import { Link } from '@/components/utility/link'
 import { useNavigation } from '@/hooks/useNavigation'
+import { Locale, Link as LocalizedLink, routing } from '@/i18n/routing'
+import { getNavigationItems, navigationConfig } from '@/lib/constants'
+import { colors } from '@/styles/config.mjs'
 
 export function Footer() {
   const t = useTranslations('common')
@@ -57,39 +58,48 @@ export function Footer() {
   return (
     <footer
       className={cn(
-        'overflow-hidden pb-12 pt-16 sm:pb-16 sm:pt-20 lg:pb-16 lg:pt-32',
-        'relative bg-gradient-appointment-reversed'
+        'pb-12 pt-16 sm:pb-16 sm:pt-20 lg:pb-16 lg:pt-32',
+        'relative overflow-hidden bg-gradient-appointment-reversed'
       )}
       ref={footerRef}
     >
       <div
-        className="pointer-events-none absolute inset-0 bg-[url('/svg/bg-footer.svg')] bg-cover bg-center"
+        className="pointer-events-none absolute inset-0 hidden bg-[url('/svg/bg-footer.svg')] bg-cover bg-center lg:block"
         style={{ mixBlendMode: 'multiply' }}
       />
-      <div className='section-container relative z-10 grid grid-cols-24 gap-y-4 pb-4'>
-        <div className='col-span-22 col-start-3'>
-          <div className='grid grid-cols-24 gap-x-8 lg:gap-x-16'>
-            {/* Logo */}
-            <div className='order-1 col-span-24 mb-8 flex items-start justify-center lg:col-span-6 lg:col-start-1 lg:mb-0 lg:justify-start'>
+      <div className='relative z-10 grid grid-cols-24 gap-y-8 pb-4'>
+        <div className='order-1 col-span-24 lg:col-span-21 lg:col-start-3 xl:col-span-21 xl:col-start-3'>
+          <div className='grid grid-cols-24'>
+            <div
+              className={cn(
+                'col-span-24 md:col-span-24 lg:col-start-1 lg:justify-start xl:col-span-6',
+                'flex items-center justify-center lg:items-start',
+                'order-1 mb-16 xl:mb-0'
+              )}
+            >
               <LocalizedLink
                 href='/'
-                className='w-[240px] sm:w-[200px] lg:w-[180px] xl:w-[260px] 2xl:w-[240px]'
+                className='w-[160px] sm:w-[200px] lg:w-[180px] xl:w-[200px] 2xl:w-[240px]'
               >
                 <Logo fill={colors['white']} />
               </LocalizedLink>
             </div>
-
-            {/* Navigation Links */}
-            <div className='order-2 col-span-24 mb-8 flex justify-between gap-8 lg:col-span-8 lg:col-start-7 lg:mb-0'>
-              <div className='flex flex-col gap-4 sm:gap-6 lg:gap-8'>
+            <div
+              className={cn(
+                'col-span-24 md:col-span-12 xl:col-span-8 xl:col-start-7',
+                'flex justify-between gap-8',
+                'order-2 mb-16 lg:mb-0',
+                'px-8 lg:px-0 lg:pr-8 xl:pr-16'
+              )}
+            >
+              <div className='flex flex-col gap-4 lg:gap-6'>
                 {getNavigationItems(t, locale as Locale)
                   .filter(item => item.mainRoute)
                   .map(item => {
                     return (
-                      <>
+                      <Fragment key={item.id}>
                         {item.id === navigationConfig['/']?.id ? (
                           <LocalizedLink
-                            key={item.id}
                             href='/'
                             className={cn(
                               'block font-primary font-[300] text-white transition-colors duration-300',
@@ -101,8 +111,7 @@ export function Footer() {
                             {item.title}
                           </LocalizedLink>
                         ) : (
-                          <LocalizedLink
-                            key={item.id}
+                          <Link
                             href={'#' + item.id}
                             className={cn(
                               'block font-primary font-[300] text-white transition-colors duration-300',
@@ -112,17 +121,17 @@ export function Footer() {
                             onClick={e => handleNavClick(e, item.id)}
                           >
                             {item.title}
-                          </LocalizedLink>
+                          </Link>
                         )}
-                      </>
+                      </Fragment>
                     )
                   })}
               </div>
-              <div className='flex flex-col gap-4 sm:gap-6 lg:gap-8'>
+              <div className='flex flex-col gap-4 lg:gap-6'>
                 {getNavigationItems(t, locale as Locale)
                   .filter(item => !item.mainRoute)
                   .map(item => (
-                    <LocalizedLink
+                    <Link
                       key={item.id}
                       href={'#' + item.href}
                       className={cn(
@@ -131,14 +140,22 @@ export function Footer() {
                       )}
                     >
                       {item.title}
-                    </LocalizedLink>
+                    </Link>
                   ))}
               </div>
             </div>
-            <div className='lg:col-start-16 order-3 col-span-24 flex flex-col gap-8 border-t border-white/50 lg:col-span-9 lg:gap-16 lg:border-l lg:border-t-0 lg:pl-16'>
-              <div className='flex flex-col'>
-                <div className='flex items-start justify-between gap-4 sm:gap-6'>
-                  <h3 className='font-primary text-xl font-[500] text-white sm:text-2xl'>
+            <div
+              className={cn(
+                'xl:col-start-16 order-3 col-span-24 md:col-span-12 xl:col-span-9',
+                'flex flex-col gap-12 lg:gap-16 xl:gap-12',
+                'border-gradient-to-b',
+                'px-8 lg:px-0 lg:pl-8 xl:pl-16'
+              )}
+            >
+              {/* sales office */}
+              <div className='order-1 flex flex-col lg:order-1'>
+                <div className='flex items-start justify-between gap-6 lg:gap-4'>
+                  <h3 className='font-primary text-xl font-[400] text-white xl:text-2xl'>
                     {t('salesOffice')}
                   </h3>
                   <div className='flex size-14 shrink-0 cursor-pointer items-center justify-center bg-bricky-brick text-white transition-colors duration-300 hover:bg-bricky-brick/80 sm:size-16 lg:size-20'>
@@ -149,21 +166,25 @@ export function Footer() {
                     />
                   </div>
                 </div>
-                <div className='-mt-6 flex flex-col gap-1 text-white sm:-mt-8'>
-                  <p className='font-primary text-sm font-[300] sm:text-base'>
+                <Link
+                  href='https://goo.gl/maps/X5VuQBQmZF2r9WZ17'
+                  className='-mt-6 flex flex-col gap-1 text-white sm:-mt-8'
+                >
+                  <p className='font-primary text-sm/[1.2] font-[300] xl:text-base'>
                     City&apos;s Istanbul AVM
                   </p>
-                  <p className='font-primary text-sm font-[300] sm:text-base'>
+                  <p className='font-primary text-sm/[1.2] font-[300] xl:text-base'>
                     İçerenköy, Çayır Caddesi No: 1, 34752
                   </p>
-                  <p className='font-primary text-sm font-[300] sm:text-base'>
+                  <p className='font-primary text-sm/[1.2] font-[300] xl:text-base'>
                     Ataşehir, İstanbul
                   </p>
-                </div>
+                </Link>
               </div>
-              <div className='flex flex-col'>
-                <div className='flex items-start justify-between gap-4 sm:gap-6'>
-                  <h3 className='font-primary text-xl font-[500] text-white sm:text-2xl'>
+              {/* contact */}
+              <div className='order-2 flex flex-col lg:order-2'>
+                <div className='flex items-start justify-between gap-6 lg:gap-4'>
+                  <h3 className='font-primary text-xl font-[400] text-white xl:text-2xl'>
                     {t('contact')}
                   </h3>
                   <div className='flex size-14 shrink-0 cursor-pointer items-center justify-center bg-bricky-brick text-white transition-colors duration-300 hover:bg-bricky-brick/80 sm:size-16 lg:size-20'>
@@ -175,93 +196,120 @@ export function Footer() {
                   </div>
                 </div>
                 <div className='-mt-6 flex flex-col gap-1 text-white sm:-mt-8'>
-                  <p className='font-primary text-sm font-[300] sm:text-base'>
+                  <Link
+                    href='mailto:info@citysresidences.com'
+                    className='block font-primary text-sm/[1.2] font-[300] xl:text-base'
+                  >
                     info@citysresidences.com
-                  </p>
-                  <p className='font-primary text-sm font-[300] sm:text-base'>
+                  </Link>
+                  <Link
+                    href='tel:+902162666600'
+                    className='block font-primary text-sm/[1.2] font-[300] xl:text-base'
+                  >
                     +90 (216) 266 66 00
-                  </p>
+                  </Link>
                 </div>
               </div>
-              <div className='flex gap-4'>
-                <div className='flex items-center justify-center'>
-                  <FacebookLogoIcon
-                    size={24}
-                    weight='fill'
-                    className='text-white'
-                  />
-                </div>
-                <div className='flex items-center justify-center'>
-                  <InstagramLogoIcon
-                    size={24}
-                    weight='fill'
-                    className='text-white'
-                  />
-                </div>
-                <div className='flex items-center justify-center'>
-                  <XLogoIcon size={24} weight='fill' className='text-white' />
-                </div>
-                <div className='flex items-center justify-center'>
-                  <YoutubeLogoIcon
-                    size={24}
-                    weight='fill'
-                    className='text-white'
-                  />
-                </div>
+              {/* social icons desktop */}
+              <div className='order-4 mx-auto hidden gap-4 lg:order-3 lg:ml-0 lg:flex'>
+                <FacebookLogoIcon weight='fill' className='size-9 text-white' />
+                <InstagramLogoIcon
+                  weight='fill'
+                  className='size-9 text-white'
+                />
+                <XLogoIcon weight='fill' className='size-9 text-white' />
+                <YoutubeLogoIcon weight='fill' className='size-9 text-white' />
               </div>
-              <div className='flex flex-col gap-6'>
-                <div className='grid grid-cols-3 gap-2 sm:gap-3 lg:gap-4'>
-                  <button className='border-gradient-soft-light bg-gradient-soft-light flex flex-col items-start justify-start gap-4 rounded-lg px-3 py-4 sm:gap-6 sm:px-4 sm:py-5 lg:gap-8 lg:px-4 lg:py-6'>
+              {/* buttons */}
+              <div className='order-3 flex flex-col gap-6 lg:order-4'>
+                <div className='grid grid-cols-3 gap-4 sm:gap-3 lg:gap-4'>
+                  <button className='border-radius-gradient flex aspect-[14/16] flex-col px-3 py-4 sm:gap-6 lg:px-4 lg:py-6'>
                     <CalendarPlusIcon
                       weight='thin'
-                      className='h-7 w-7 text-white sm:h-8 sm:w-8 lg:h-9 lg:w-9'
+                      className='size-8 text-white sm:h-8 sm:w-8 lg:h-9 lg:w-9'
                     />
-                    <span className='text-left font-primary text-xs font-[400] leading-tight text-white sm:text-sm lg:text-base'>
-                      {t('createAppointment')}
+                    <span className='mt-auto text-left font-primary text-sm font-[400] leading-tight text-white sm:text-sm lg:text-base'>
+                      {t.rich('createAppointment', {
+                        br: () => <br />,
+                      })}
                     </span>
                   </button>
-                  <button className='border-gradient-soft-light bg-gradient-soft-light flex flex-col items-start justify-start gap-4 rounded-lg px-3 py-4 sm:gap-6 sm:px-4 sm:py-5 lg:gap-8 lg:px-4 lg:py-6'>
-                    <PhoneCallIcon
+                  <button className='border-radius-gradient flex aspect-[14/16] flex-col px-3 py-4 sm:gap-6 lg:px-4 lg:py-6'>
+                    <HeadsetIcon
                       weight='thin'
-                      className='h-7 w-7 text-white sm:h-8 sm:w-8 lg:h-9 lg:w-9'
+                      className='size-8 text-white sm:h-8 sm:w-8 lg:h-9 lg:w-9'
                     />
-                    <span className='text-left font-primary text-xs font-[400] leading-tight text-white sm:text-sm lg:text-base'>
-                      {t('speakWithRepresentative')}
+                    <span className='mt-auto text-left font-primary text-sm font-[400] leading-tight text-white sm:text-sm lg:text-base'>
+                      {t.rich('speakWithRepresentative', {
+                        br: () => <br />,
+                      })}
                     </span>
                   </button>
-                  <button className='border-gradient-soft-light bg-gradient-soft-light flex flex-col items-start justify-start gap-4 rounded-lg px-3 py-4 sm:gap-6 sm:px-4 sm:py-5 lg:gap-8 lg:px-4 lg:py-6'>
+                  <Link
+                    href='https://goo.gl/maps/X5VuQBQmZF2r9WZ17'
+                    className='border-radius-gradient flex aspect-[14/16] flex-col px-3 py-4 sm:gap-6 lg:px-4 lg:py-6'
+                  >
                     <MapPinPlusIcon
                       weight='thin'
-                      className='h-7 w-7 text-white sm:h-8 sm:w-8 lg:h-9 lg:w-9'
+                      className='size-8 text-white sm:h-8 sm:w-8 lg:h-9 lg:w-9'
                     />
-                    <span className='text-left font-primary text-xs font-[400] leading-tight text-white sm:text-sm lg:text-base'>
-                      {t('getDirections')}
+                    <span className='mt-auto text-left font-primary text-sm font-[400] leading-tight text-white sm:text-sm lg:text-base'>
+                      {t.rich('getDirections', {
+                        br: () => <br />,
+                      })}
                     </span>
-                  </button>
+                  </Link>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className='col-span-22 col-start-3 mt-8 flex flex-col gap-4 text-sm text-white/50 sm:text-base lg:mt-auto lg:flex-row lg:justify-between lg:text-lg'>
-          <span className='flex flex-col font-primary font-[300]'>
+        {/* social icons mobile */}
+        <div className='order-2 col-span-24 mt-12 flex lg:hidden'>
+          <div className='mx-auto flex gap-4'>
+            <FacebookLogoIcon weight='fill' className='size-9 text-white' />
+            <InstagramLogoIcon weight='fill' className='size-9 text-white' />
+            <XLogoIcon weight='fill' className='size-9 text-white' />
+            <YoutubeLogoIcon weight='fill' className='size-9 text-white' />
+          </div>
+        </div>
+        <div
+          className={cn(
+            'order-3 lg:order-2',
+            'mt-8 lg:mt-auto',
+            'col-span-24 lg:col-span-21 lg:col-start-3 xl:col-span-21 xl:col-start-3',
+            'flex flex-col gap-4 lg:flex-row lg:justify-between',
+            'text-sm text-white/90',
+            'font-primary font-[300]'
+          )}
+        >
+          <span className='flex flex-col gap-4 text-center lg:gap-0 lg:text-left'>
             <span>2025 ©</span>
             <span>City&apos;s Residences İstanbul Tüm hakları saklıdır.</span>
           </span>
         </div>
-        <div className='col-span-22 col-start-3 flex flex-col items-center gap-4 text-sm text-white/50 sm:flex-row sm:justify-between sm:text-base lg:text-lg'>
+        <div
+          className={cn(
+            'order-2 lg:order-3',
+            'mt-8 lg:mt-auto',
+            'col-span-24 lg:col-span-21 lg:col-start-3 xl:col-span-21 xl:col-start-3',
+            'flex flex-col gap-4 lg:flex-row xl:justify-between',
+            'text-sm text-white/90',
+            'font-primary font-[300]'
+          )}
+        >
           <span className='hidden lg:inline'>{t('copyright')}</span>
-          <span className='flex flex-wrap justify-center gap-4 sm:gap-6 lg:gap-8'>
+          <span className='ml-auto flex flex-col flex-wrap items-center justify-center gap-6 md:gap-6 lg:flex-row lg:items-start lg:gap-6'>
             {footerItems.legal.map((item, i) => (
-              <LocalizedLink
+              <Link
                 target='_blank'
                 rel='noopener noreferrer'
                 key={i}
                 href={item.href}
-                className={cn('flex-shrink-0', 'block')}
+                className='block flex-shrink-0 text-tangerine-flake'
               >
                 {item.title}
-              </LocalizedLink>
+              </Link>
             ))}
           </span>
         </div>
