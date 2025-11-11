5655f945c589f2160c1732e516b1d0d7be70085e
armagansenol <armagansnl@gmail.com>
2025-11-11 11:10:59 +0300

    fix: update font paths for Suisse Intl in layout component
    - Correct the paths for Suisse Intl font files in the layout component to ensure proper loading.
    - This change enhances the accessibility of the font assets by aligning the paths with the project structure.

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -16,47 +16,47 @@ import { WebChat } from '@/components/web-chat'
 const suisseIntl = localFont({
   src: [
     // {
-    //   path: './fonts/suisse-intl/SuisseIntl-UltraLight.woff2',
+    //   path: '../fonts/suisse-intl/SuisseIntl-UltraLight.woff2',
     //   weight: '100',
     //   style: 'normal',
     // },
     {
-      path: './fonts/suisse-intl/SuisseIntl-Thin.woff2',
+      path: '../fonts/suisse-intl/SuisseIntl-Thin.woff2',
       weight: '200',
       style: 'normal',
     },
     {
-      path: './fonts/suisse-intl/SuisseIntl-Light.woff2',
+      path: '../fonts/suisse-intl/SuisseIntl-Light.woff2',
       weight: '300',
       style: 'normal',
     },
     // {
-    //   path: './fonts/suisse-intl/SuisseIntl-Book.woff2',
+    //   path: '../fonts/suisse-intl/SuisseIntl-Book.woff2',
     //   weight: '350',
     //   style: 'normal',
     // },
     {
-      path: './fonts/suisse-intl/SuisseIntl-Regular.woff2',
+      path: '../fonts/suisse-intl/SuisseIntl-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: './fonts/suisse-intl/SuisseIntl-Medium.woff2',
+      path: '../fonts/suisse-intl/SuisseIntl-Medium.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: './fonts/suisse-intl/SuisseIntl-Semibold.woff2',
+      path: '../fonts/suisse-intl/SuisseIntl-Semibold.woff2',
       weight: '600',
       style: 'normal',
     },
     {
-      path: './fonts/suisse-intl/SuisseIntl-Bold.woff2',
+      path: '../fonts/suisse-intl/SuisseIntl-Bold.woff2',
       weight: '700',
       style: 'normal',
     },
     // {
-    //   path: './fonts/suisse-intl/SuisseIntl-Black.woff2',
+    //   path: '../fonts/suisse-intl/SuisseIntl-Black.woff2',
     //   weight: '900',
     //   style: 'normal',
     // },
