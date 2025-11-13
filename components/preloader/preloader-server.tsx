12548f4f7554700238bd8cd01e5d18a14305d232
armagansenol <armagansnl@gmail.com>
2025-11-13 13:23:12 +0300

    refactor: standardize PreloaderServer component styling and improve code consistency
    - Update string quotes from double to single for consistency across the PreloaderServer component.
    - Rearrange class names for better readability while maintaining functionality.

--- components/preloader/preloader-server.tsx
+++ components/preloader/preloader-server.tsx
@@ -1,18 +1,18 @@
-import { Img } from "@/components/utility/img"
+import { Img } from '@/components/utility/img'
 
 export function PreloaderServer() {
-  if (process.env.NODE_ENV !== "production") {
+  if (process.env.NODE_ENV !== 'production') {
     return null
   }
 
   return (
     <div
-      id="server-preloader"
-      className="z-[var(--z-preloader)] fixed top-0 left-0 w-full h-full overflow-hidden bg-bricky-brick flex items-center justify-center pointer-events-none"
+      id='server-preloader'
+      className='pointer-events-none fixed left-0 top-0 z-[var(--z-preloader)] flex h-full w-full items-center justify-center overflow-hidden bg-bricky-brick'
     >
-      <div className="w-32 h-32 lg:w-80 lg:h-80">
+      <div className='size-32'>
         <Img
-          src="/gif/citys-logo-animation.gif"
+          src='/gif/citys-logo-animation.gif'
           alt="City's Residences Logo Animation"
           width={256}
           height={256}
