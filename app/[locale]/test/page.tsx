b3d753c0c4f3b931765fcb03569767640a90a12d
armagansenol <armagansnl@gmail.com>
2026-02-03 13:19:42 +0300

    refactor: simplify page component structure and remove unused imports
    - Removed the Wrapper component and associated countries fetching from the Page component, streamlining the structure.
    - This change enhances readability and maintainability by eliminating unnecessary code while preserving the overall functionality.

--- app/[locale]/test/page.tsx
+++ app/[locale]/test/page.tsx
@@ -1,10 +1,8 @@
 import dynamic from 'next/dynamic'
 
-import { Wrapper } from '@/components/wrapper'
 import Home from '@/components/sections/home'
 import ProjectSection from '@/components/sections/project'
 import ResidencesSection from '@/components/sections/residences'
-import { getCountries } from '@/lib/api/countries'
 
 const CitysIstanbulAvm = dynamic(
   () => import('@/components/sections/citys-istanbul-avm')
@@ -12,17 +10,13 @@ const CitysIstanbulAvm = dynamic(
 const CitysTimes = dynamic(() => import('@/components/sections/citys-times'))
 
 export default async function Page({ params }: { params: { locale: string } }) {
-  const countries = getCountries()
-
   return (
     <>
-      <Wrapper countries={countries}>
-        <Home params={params} />
-        <ProjectSection params={params} />
-        <ResidencesSection params={params} />
-        <CitysIstanbulAvm />
-        <CitysTimes locale={params.locale} />
-      </Wrapper>
+      <Home params={params} />
+      <ProjectSection params={params} />
+      <ResidencesSection params={params} />
+      <CitysIstanbulAvm />
+      <CitysTimes locale={params.locale} />
     </>
   )
 }
